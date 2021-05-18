6229ebd93d4fbe3509ed1c8642d267dcdf097656
ZeroMarker <u78k4l67@163.com>
2021-05-18 18:43:16 +0800

    game

---
+++ java/str.java
@@ -0,0 +1,22 @@
+// test 13
+import java.util.Scanner;
+public class str{
+    public static void main(String[] args){
+        
+        String str1 = "Hello World";
+        String str2 = "Hello World!";
+        String str3 = " Hello World ";
+        
+        System.out.println(str1.compareTo(str2));
+        System.out.println(str1.indexOf("H"));
+        System.out.println(str1.replace("H", "Y"));
+        System.out.println(str1.toLowerCase());
+        System.out.println(str3.trim());
+
+        StringBuffer strb = new StringBuffer(str1);
+        strb.append("!!!");
+        System.out.println(strb);
+        strb.reverse();
+        System.out.println(strb);
+    }
+}
